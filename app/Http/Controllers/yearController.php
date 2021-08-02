06ab0c3b100f49904788b13b0957826db84ebc40
Jeroen Cuvelier <jeroen.cuvelier@live.be>
2021-08-02 13:57:21 +0200

    2021 code cleanup

--- app/Http/Controllers/yearController.php
+++ app/Http/Controllers/yearController.php
@@ -8,51 +8,33 @@ use DateTime;
 
 class YearController extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {}
-
     public function getPayDays($year, $outputType = 'csv') {
         $yearMonths = [];
         $payDays    = [];
         $bonusDays  = [];
+
         for ($i = 1; $i <= 12; $i++) {
-            $lastDay = date("Y/m/t", strtotime($year."-".$i."-1"));
+            $lastDay = date("Y/m/t", strtotime($year . "-" . $i . "-1"));
             array_push($payDays, $lastDay);
-            array_push($bonusDays, $year."/".$i."/15");
-        }
-        foreach($payDays as $key => $day){
-            $dayOfWeek = date('w', strtotime($day));
-            switch($dayOfWeek) {
-                case 6:
-                    $payDays[$key] = date('Y/m/d',(strtotime ( '-1 day' , strtotime ( $day))));
-                    break;
-                case 0:
-                    $payDays[$key] = date('Y/m/d',(strtotime ( '-2 days' , strtotime ( $day))));
-                    break;
+            if ($i === 12) {
+                $nextYear = $year + 1;
+                array_push($bonusDays, $nextYear . "/1/15");
+            }
+            else {
+                $nextMonth = $i + 1;
+                array_push($bonusDays, $year . "/" . $nextMonth . "/15");
             }
         }
-        foreach($bonusDays as $key => $day){
-            $dayOfWeek = date('w', strtotime($day));
-            switch($dayOfWeek) {
-                case 6:
-                    $bonusDays[$key] = date('Y/m/d',(strtotime ( '+4 days' , strtotime ( $day))));
-                    break;
-                case 0:
-                    $bonusDays[$key] = date('Y/m/d',(strtotime ( '+3 days' , strtotime ( $day))));
-                    break;
-            }
-        }
+
+        $this->processPaydays($payDays);
+        $this->processBonusdays($bonusDays);
 
         if ($outputType === 'json') {
             return response()->json(['year' => $year, 'payDays' => $payDays, 'bonusDays' => $bonusDays]);
         }
 
-        $headers = $this->getFileResponseHeaders($year.'.csv');
+        $filename = $year.'.csv';
+        $headers = $this->getFileResponseHeaders($filename);
         $columns = ["month", "Payday", "Bonus Payday"];
 
         return $this->streamFile(function () use ($payDays, $bonusDays, $columns) {
@@ -72,7 +54,7 @@ class YearController extends Controller
         }, $headers);
     }
 
-    protected function streamFile($callback, $headers)
+    private function streamFile($callback, $headers)
     {   
         try {
             $response = new StreamedResponse($callback, 200, $headers);
@@ -83,11 +65,46 @@ class YearController extends Controller
         }
     }
 
-    protected function getFileResponseHeaders($filename)
+    private function getFileResponseHeaders($filename)
     {
         return [
             'Content-Type'        => 'text/csv',
             'Content-Disposition' => 'attachment; filename='.$filename,
         ];
+    }
+
+    private function processPayDays(&$payDays): void
+    {
+        //if the payday is a weekend day, we take the previous Friday (PHP has Sunday as day 0)
+        $cases = [
+            0 => '-2 days',
+            6 => '-1 day'
+        ];
+        $this->processDays($payDays, $cases);
+    }
+
+    private function processBonusDays(&$bonusDays): void
+    {
+        //if the 15th is a weekend day, we take the next Wednesday (PHP has Sunday as day 0)
+        $cases = [
+            0 => '+3 days',
+            6 => '+4 days'
+        ];
+        $this->processDays($bonusDays, $cases);
+    }
+
+    private function processDays(&$days, $cases): void
+    {
+        foreach($days as $key => $day){
+            $dayOfWeek = date('w', strtotime($day));
+            switch($dayOfWeek) {
+                case 6:
+                    $days[$key] = date('Y/m/d',(strtotime ( $cases[6] , strtotime ( $day))));
+                    break;
+                case 0:
+                    $days[$key] = date('Y/m/d',(strtotime ( $cases[0] , strtotime ( $day))));
+                    break;
+            }
+        }
     }
 }
